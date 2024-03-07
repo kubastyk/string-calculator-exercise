672c5989812f082810fff069039a95f9b9b13bd3
Jakub Stawiszynski <stawiszynskijakub@gmail.com>
2024-03-07 10:39:14 +0100

    Add basic tests for adding valid numbers

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,8 +1,32 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
 
+    private static Calculator calculator;
+
+    @BeforeAll
+    public static void beforeAll() {
+        calculator = new Calculator();
+    }
+
     @Test
-    void add() {
+    void add_emptyString_returnZero() {
+        int result = calculator.add("");
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void add_oneNumberString_returnSameNumber() {
+        int result = calculator.add("1");
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void add_multipleValidNumbersString_returnSum() {
+        int result = calculator.add("1,2");
+        assertEquals(3, result);
     }
 }
