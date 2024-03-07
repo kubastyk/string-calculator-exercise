827025b2720d2325b2c10a141adf9e2c109f7b3f
Jakub Stawiszynski <stawiszynskijakub@gmail.com>
2024-03-07 11:57:45 +0100

    Fix criteria for throwing exception when delimiter ends input

--- src/main/java/services/InputValidationService.java
+++ src/main/java/services/InputValidationService.java
@@ -4,10 +4,11 @@ import exceptions.IncorrectInputFormatException;
 
 public class InputValidationService {
 
-    private static final String DEFAULT_DELIMITERS = ",|\n";
+    private static final String COMMA_DELIMITER = ",";
+    private static final String NEW_LINE_DELIMITER = "\n";
 
     public static void verifyIfInputEndsWithDelimiter(String input) throws IncorrectInputFormatException {
-        if(input.endsWith(DEFAULT_DELIMITERS))
+        if(input.endsWith(COMMA_DELIMITER) || input.endsWith(NEW_LINE_DELIMITER))
             throw new IncorrectInputFormatException("Input data cannot end with the separator");
     }
 }
