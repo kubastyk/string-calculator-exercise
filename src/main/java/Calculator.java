6147e8352d57a3873fed7b9a5eead070b7a8d54f
Jakub Stawiszynski <stawiszynskijakub@gmail.com>
2024-03-07 10:43:34 +0100

    Implement basic scenario for adding valid numbers

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,7 +1,18 @@
+import java.util.Arrays;
+
 public class Calculator {
 
     public int add(String numbers) {
+        int result = 0;
 
-        return 0;
+        if(numbers.length() == 0)
+            return 0;
+
+        String[] splitNumbers = numbers.split(",");
+        result = Arrays.stream(splitNumbers)
+                .mapToInt(num -> Integer.parseInt(num))
+                .reduce(0, Integer::sum);
+
+        return result;
     }
 }
