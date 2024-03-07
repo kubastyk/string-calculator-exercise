2a8a9761004bbda4cfda65dedd7aac226183ce7d
Jakub Stawiszynski <stawiszynskijakub@gmail.com>
2024-03-07 11:05:14 +0100

    Implement multiple argument add()

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -2,6 +2,10 @@ import java.util.Arrays;
 
 public class Calculator {
 
+    public int add(String... args) {
+        return Arrays.stream(args).mapToInt(this::add).sum();
+    }
+
     public int add(String numbers) {
         int result = 0;
 
