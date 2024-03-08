3685333bdc5601bc7f96a3b774b38a531f9e44a9
Jakub Stawiszynski <stawiszynskijakub@gmail.com>
2024-03-08 15:08:41 +0100

    main() - create example run + describe problem in docs

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,20 @@
-import exceptions.IncorrectInputFormatException;
 import services.CalculatorService;
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            var calculator = new CalculatorService();
-            calculator.add("//|\n1|2,-3");
-        } catch(IncorrectInputFormatException e) {
-            System.out.println(e.getMessage());
-        }
+        var calculator = new CalculatorService();
+
+        /**
+         * I think there might be a logical issue at point 7 of the exercise:
+         * Example input: "//|\n1|2,-3"
+         * - in this case we getting information that the separator is '|'
+         * - when we split the data by this separator we receive: '1' & '2,-3'
+         * - since we should treat second value as a result of separation by diameter
+         *   we are not able to display information about negative integer since it is not a number
+         */
+        calculator.add("//|\n1|2,-3");
+        System.out.println("---Valid example of displaying multiple exception messages---");
+        calculator.add("//|\n1|2|-3|");
     }
 }
