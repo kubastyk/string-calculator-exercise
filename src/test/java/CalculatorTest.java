33e146e5cd74feffec9b650f13f729831f808caa
Jakub Stawiszynski <stawiszynskijakub@gmail.com>
2024-03-08 09:34:16 +0100

    Remove unused imports from test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,7 @@
-import exceptions.IncorrectInputFormatException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CalculatorTest {
 
